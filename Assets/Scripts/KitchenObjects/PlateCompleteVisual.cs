757f15be47ab306986a683de3dbbe7bf5ce3b943
Dennis Federico <dennisfederico@gmail.com>
2023-03-22 19:02:06 +0100

    fixed ingredients not showing on plate in final build. clean-up

--- Assets/Scripts/KitchenObjects/PlateCompleteVisual.cs
+++ Assets/Scripts/KitchenObjects/PlateCompleteVisual.cs
@@ -1,18 +1,31 @@
-using AYellowpaper.SerializedCollections;
+using System;
+using System.Collections.Generic;
 using ScriptableObjects;
 using UnityEngine;
 
 namespace KitchenObjects {
     public class PlateCompleteVisual : MonoBehaviour {
+        
+        [Serializable]
+        public struct ScriptableToPrefabKitchenObject {
+            public KitchenObjectScriptable key;
+            public GameObject value;
+        }
+        
         [SerializeField] private PlateKitchenObject plateKitchenObject;
-        [SerializeField] private SerializedDictionary<KitchenObjectScriptable, GameObject> kitchenObjectScriptableVisualMap = new();
 
+        [SerializeField] private List<ScriptableToPrefabKitchenObject> scriptableToVisualList = new();
+        private readonly Dictionary<KitchenObjectScriptable, GameObject> _kitchenObjectScriptableVisualMap = new();
+        
         private void Start() {
             plateKitchenObject.AddedIngredient += PlateKitchenObjectOnAddedIngredient;
+            foreach (var item in scriptableToVisualList) {
+                _kitchenObjectScriptableVisualMap.Add(item.key, item.value);
+            }
         }
 
         private void PlateKitchenObjectOnAddedIngredient(KitchenObjectScriptable ingredient) {
-            if (kitchenObjectScriptableVisualMap.TryGetValue(ingredient, out var ingredientVisual)) {
+            if (_kitchenObjectScriptableVisualMap.TryGetValue(ingredient, out var ingredientVisual)) {
                 ingredientVisual.SetActive(true);
             }
         }
